c6ff53feab9b60a65924b3bbb690e5ce7b4264ff
InSeok <129822581+InSeok211@users.noreply.github.com>
2025-12-22 03:26:43 +0900

    Delete wp-term-project-v13 directory

--- wp-term-project-v13/src/main/java/com/wplab/service/AdminDAO.java
+++
@@ -1,7 +0,0 @@
-package com.wplab.service;
-
-import java.sql.SQLException;
-
-public interface AdminDAO {
-    AdminDO getAdminByUsernameAndPassword(String admin_id, String password) throws SQLException;
-}
